4b80dea0e7d371e415d3c797a0a610a47bdbfe61
emoomey <emoomey@gmail.com>
2025-12-24 16:57:05 -0800

    Update tailwind.config.js

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-green-600',
+    'text-white',
+    'rounded-lg',
+    'p-4',
+    'min-h-screen',
+    'flex',
+    'items-center',
+    'justify-center',
+  ],
   theme: {
     extend: {
       screens: {
